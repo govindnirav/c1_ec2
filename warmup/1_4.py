4d949bb58f02a1ce0ddd5503e839a91fbf61e982
govindnirav <87795211+govindnirav@users.noreply.github.com>
2024-10-30 16:37:11 +0000

    1.4 update

---
+++ warmup/1_4.py
@@ -0,0 +1,17 @@
+
+def recaman_sq(n):
+    out=[]
+    x=0
+    for i in range (0, n):
+        if x==0 and x not in out:
+            out.append(x)
+        elif (x-i)>0 and x-i not in out:
+            x = x-i
+            out.append(x)
+        else:
+            x = x+i
+            out.append(x)
+    return out
+
+n=100
+print(recaman_sq(n))
