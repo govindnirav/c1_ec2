c8751afb33ca1f8dc7d3eeb3e8b7515e4f964a98
govindnirav <87795211+govindnirav@users.noreply.github.com>
2024-10-30 17:32:59 +0000

    1.6 correction

--- warmup/1_6.py
+++ warmup/1_6.py
@@ -1,3 +1,3 @@
 N=362880
-factor=[(x,N//x) for x in range(1,N//2 + 1) if N%x==0]
-print(factor) #Technically prints out double the amount, but if the order of x and y matter then this is necessary
+factor=[(x,N//x) for x in range(1,int(N**0.5) + 1) if N%x==0] ##Using the sqrt to avoid mirroring pairs
+print(factor)
