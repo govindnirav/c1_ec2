82aca75fe8a97143d19bb68737b939e1883cc68c
govindnirav <87795211+govindnirav@users.noreply.github.com>
2024-10-30 15:55:02 +0000

    changes to 1.3

--- warmup/1_3.py
+++ warmup/1_3.py
@@ -1,7 +1,10 @@
 def prime_list(n):
     """
     Prints a list of prime numbers smaller than input integer, N. 
-    I do not consider 1 a prime number. Using the Sieve of Erathosthenes
+    I do not consider 1 a prime number. Using the Sieve of Erathosthenes:
+    Essentially creates a list of length n+1 (n+1 for indexing purposes)
+    For each value, p, in sieve, it checks if p==TRUE. If so, it appends p to out.
+    It also turns every multiple of p in sieve into false. (A multiple can't be a prime number)
     """
     out=[]
     sieve = [True] * (n+1)
