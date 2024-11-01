41ceb30e81cec89ea7a28ce0b7e0061d590a96f0
govindnirav <87795211+govindnirav@users.noreply.github.com>
2024-11-01 07:36:13 +0000

    Update

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-## Defining parameters
-# drift coefficent
-MU = 0.1
-# number of steps
-N = 100
-# time in years
-T = 1
-# number of sims
-M = 100
-# initial stock price
-Y0 = 100
-# volatility
-SIGMA = 0.3
+import pygbm.gbm as pg
 
